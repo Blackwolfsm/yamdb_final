b6c90f4cee4215215e911c5f1ec3f72a96ce5ceb
Mokrov Sergey <blackwolfsm@gmail.com>
2020-12-09 00:26:34 +0500

    repair for tests

--- api/views.py
+++ api/views.py
@@ -10,7 +10,6 @@ from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.serializers import ValidationError
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from api_yamdb.utilities import get_user_confirmation_code
